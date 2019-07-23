f3b991cab368743b7a6880ba53068bd45c1421fc
Sandra Marcela Llanos Alvarez <sandra.llanos@sophos.col.com>
2019-07-23 16:58:15 -0500

    first commit angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ConversationComponent } from './conversation/conversation.component';
+import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent  },
+  {path: 'home', component: HomeComponent  },
+  {path: 'login', component: LoginComponent  },
+  {path: 'profile', component: ProfileComponent  },
+  {path: 'conversation', component: ConversationComponent  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    LoginComponent,
+    HomeComponent,
+    ConversationComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user';
+import { getMaxListeners } from 'cluster';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { 
+    let myUser: User = {
+      nick: 'eduardo',
+      subnick: 'Hola',
+      age: 12,
+      email: 'sa@gmail.com',
+      friend: true,
+      uid: 1
+    };
+    console.log(myUser);
+  }
+
+  ngOnInit() {
+  }
+
+}
